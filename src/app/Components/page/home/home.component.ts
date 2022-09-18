2d5114ec7d42d61eb36cf3a203595009508d3355
bmiomi <bryanmiomi@hotmail.com>
2022-09-18 14:07:54 -0500

    2 files modified

--- src/app/Components/page/home/home.component.ts
+++ src/app/Components/page/home/home.component.ts
@@ -3,15 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
-    col:string='danger'
+  
+  col: string = 'black';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
